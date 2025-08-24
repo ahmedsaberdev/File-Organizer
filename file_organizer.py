207fdcb02fb72a0291fc5f4576796aaec89b518c
Ahmed Saber <ahmed.saber.dev.05@gmail.com>
2025-08-24 07:09:54 +0300

    feature: Add core features for creating directories (if not existent) and organize files based on thier extentions.

---
+++ file_organizer.py
@@ -0,0 +1,42 @@
+import shutil
+from pathlib import Path
+
+CATEGORIES = {
+  "Images" : [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp"],
+  "Videos" : [".mp4", ".mkv", ".mov", ".avi"],
+  "Documents" : [".pdf", ".docx", ".txt", ".xlsx", ".pptx"],
+  "Music" : [".mp3", ".wav", ".flac", ".ogg"],
+  "Archives" : [".zip", ".rar", ".tar", ".gz"],
+  "Scripts" : [".py", ".js", ".sh"],
+  "Source Files" : [".cpp", ".c", ".java"],
+  "Applications" : [".exe"],
+  "Torrents" : [".torrent"],
+  "Others" : []
+}
+
+TARGET_FOLDER = r'C:\Users\Queen\Desktop\Lock in\AI & ML\Python\Train\File organizer\Test folder'
+
+def folder_organizer(folder):
+  for entry in folder.iterdir():
+    if entry.is_file():
+      ext = entry.suffix.lower()
+      moved = False
+
+      for category, extentions in CATEGORIES.items():
+        if ext in extentions:
+          move_file(entry, folder / category)
+          moved = True
+          break
+
+      if not moved:
+        move_file(entry, folder / "Others")
+
+def move_file(file, dest_folder):
+  dest_folder.mkdir(exist_ok=True)
+  shutil.move(str(file), str(dest_folder / file.name))
+  print(f'Moved {file.name} -> {dest_folder.name}')
+
+
+if __name__ == "__main__":
+  target = Path(TARGET_FOLDER)
+  folder_organizer(target)
